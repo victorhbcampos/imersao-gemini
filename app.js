87293fcfebc49ed108aafac338da873c681fa286
Victor Hugo Borges Campos <victor.hugoborcas18@gmail.com>
2024-09-06 10:09:15 -0300

    parte_final

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
 function pesquisar() {
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById('resultados-pesquisa')
+
+    let campoPesquisa = document.getElementById('campo-pesquisa').value
+
+    if (!campoPesquisa) {
+        section.innerHTML = "<p class='mensagem-erro'>Informe o nome ou nacionalidade de algum piloto.</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
   
     // Inicializa uma string vazia para armazenar os resultados formatados
     let resultados = ""
+    let nome = ""
+    let descricao = ""
   
     // Itera sobre cada item de dados e constrói o HTML para cada resultado
     for (let dado of dados) {
-      // Cria um novo elemento de resultado com as informações do dado atual
-      resultados += `
-        <div class="item-resultado">
-          <h2>
-            <a href="#" target="_blank">${dado.nome}</a>
-          </h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a href="${dado.link}" target="_blank">Mais informações</a>
-        </div>
-      `
+        nome = dado.nome.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        if(nome.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+            // Cria um novo elemento de resultado com as informações do dado atual
+            resultados += `
+            <div class="item-resultado">
+              <h2>
+                <a href="#" target="_blank">${dado.nome}</a>
+              </h2>
+              <h3>Títulos: ${dado.titulos}</h3>
+              <p class="descricao-meta">${dado.descricao}</p>
+              <a href="${dado.link}" target="_blank">Mais informações</a>
+            </div>
+          `
+        }
+
+      
+    }
+
+    if (!resultados) {
+        resultados = "<p class='mensagem-erro'>Nenhum piloto foi encontrado.</p>"
     }
   
     // Atualiza o conteúdo da seção HTML com os resultados construídos
     section.innerHTML = resultados
-  }
+}
--- dados.js
+++ dados.js
@@ -16,5 +16,23 @@ let dados = [
         descricao: "Emerson Fittipaldi foi o primeiro piloto brasileiro a conquistar o título mundial de Fórmula 1. Bicampeão mundial, Fittipaldi é considerado um dos maiores pilotos da história do automobilismo brasileiro.",
         link: "https://pt.wikipedia.org/wiki/Emerson_Fittipaldi",
         titulos: 2
+    },
+    {
+        nome: "Michael Schumacher",
+        descricao: "Michael Schumacher é considerado um dos maiores pilotos de Fórmula 1 de todos os tempos. Com sete títulos mundiais, o alemão dominou a categoria por diversas temporadas.",
+        link: "https://pt.wikipedia.org/wiki/Michael_Schumacher",
+        titulos: 7
+    },
+    {
+        nome: "Lewis Hamilton",
+        descricao: "Lewis Hamilton é um piloto de Fórmula 1 britânico, sete vezes campeão mundial. Conhecido por sua habilidade e rivalidade com outros grandes pilotos, Hamilton é um dos maiores nomes da Fórmula 1 na atualidade.",
+        link: "https://pt.wikipedia.org/wiki/Lewis_Hamilton",
+        titulos: 7
+    },
+    {
+        nome: "Max Verstappen",
+        descricao: "Max Verstappen é um piloto de Fórmula 1 holandês, tricampeão mundial. Conhecido por seu estilo de pilotagem agressivo e pelas disputas acirradas, Verstappen é um dos grandes nomes da nova geração da Fórmula 1.",
+        link: "https://pt.wikipedia.org/wiki/Max_Verstappen",
+        titulos: 3
     }
 ];
